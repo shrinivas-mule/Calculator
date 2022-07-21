2e3f3358875c05410bb93f4bdffcfd18e18c39c6
shrinivas-mule <shmule97@gmail.com>
2022-07-21 13:03:03 +0530

    AdvancedCalculator is saved

---
+++ Calculator/AdavnceCalculator.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+class Main {
+
+
+  public static void main(String[] args) {
+
+    char operator;
+    Double number1, number2, result;
+
+    // create an object of Scanner class
+    Scanner input = new Scanner(System.in);
+
+    // ask users to enter operator
+   // System.out.println("Choose an operator: +, -, *, or /");
+   System.out.println("Choose an operator: +, -, *,/,%");
+
+    operator = input.next().charAt(0);
+
+    // ask users to enter numbers
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+
+
+    switch (operator) {
+
+      // performs addition between numbers
+      case '+':
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
+
+      // performs subtraction between numbers
+      case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      // performs multiplication between numbers
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+     
+      case '%':
+        System.out.println("enter per value");
+       Double per=input.nextDouble();
+        result = (number1*per)/100;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      
+   
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}
